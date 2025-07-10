4ee82e5f68c1209f28358566aaeab36b1ee3a69f
sangwon5579 <chosw0410@naver.com>
2025-07-10 15:11:46 +0900

    Merge branch 'main' of https://github.com/sangwon5579/Study_JAVA

--- 01_Variable/src/com/beyond/variable/Application.java
+++ 01_Variable/src/com/beyond/variable/Application.java
@@ -1,9 +1,14 @@
+//패키지(Package)
+//java에서 클래스를 관리하기 위해 패키지를 사용한다.
+//폴더를 만들어서 파일을 저장하고 관리하듯이 패키지를 만들어 클래스를 저장하고 관리한다.
 package com.beyond.variable;
 //블록 주석 : 컨트롤 + /
+
+//클래스의 이름은 java 파일의  이름과 동일
 public class Application {
+    //main 메소드
+    //프로젝트의 시작점. java.exe가 Application.class를 실행시킬 때 제일 먼저 실행되는 메소드.
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Hello World");
         System.out.println("Hello World");
     }
 }
