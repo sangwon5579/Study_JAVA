bcd9d65356b36d657a4be416de0e628a2a44abef
sangwon5579 <81066249+sangwon5579@users.noreply.github.com>
2025-07-11 14:17:56 +0900

    Delete 02_Operator/src/com/beyond/operator directory

--- 02_Operator/src/com/beyond/operator/Application.java
+++
@@ -1,9 +0,0 @@
-package com.beyond.operator;
-
-import com.beyond.operator.practice.A_logicalNegation;
-
-public class Application {
-    public static void main(String[] args) {
-        new A_logicalNegation().method1();
-    }
-}
