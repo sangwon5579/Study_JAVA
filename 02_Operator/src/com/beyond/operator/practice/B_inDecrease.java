ba5193499d3f08110af865319a9041eb3b241d1d
sangwon5579 <chosw0410@naver.com>
2025-07-11 14:30:14 +0900

    Day02

--- 02_Operator/src/com/beyond/operator/practice/B_inDecrease.java
+++ 02_Operator/src/com/beyond/operator/practice/B_inDecrease.java
@@ -1,5 +1,21 @@
 package com.beyond.operator.practice;
 
 public class B_inDecrease {
+    public void method1() {
+        int number = 10;
 
+        //전위연산
+        System.out.println("num : " + number);
+        System.out.println("1회수행 후" + ++number);
+        System.out.println("num : " + number);
+        System.out.println();
+
+        //후위연산
+        System.out.println("num : " + number);
+        System.out.println("1회수행 후" + number--);
+        System.out.println("num : " + number);
+        System.out.println();
+        System.out.println();
+
+    }
 }
