9d3a69ad27db64c140779a0433f51887dab0727a
sangwon5579 <chosw0410@naver.com>
2025-07-11 16:58:00 +0900

    DAY02

--- 02_Operator/src/com/beyond/operator/practice/F_Assignment.java
+++ 02_Operator/src/com/beyond/operator/practice/F_Assignment.java
@@ -5,7 +5,20 @@ public class F_Assignment {
         int number = 12;
 
         number += 3;
+        System.out.println(number); //15
 
-        System.out.println(number);
+        number -= 3;
+        number *= 2;
+        System.out.println(number); //24
+
+        number /= 2;
+        System.out.println(number); //12
+
+        number %= 2;
+        System.out.println(number); //0
+
+        String str = "Hello";
+        str += "World";
+        System.out.println("str += \"World\"은 : " + str);
     }
 }
