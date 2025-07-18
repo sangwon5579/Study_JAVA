d793dd8ac9036b6042d3bc6651eae1b0eb02062e
sangwon5579 <chosw0410@naver.com>
2025-07-18 10:41:32 +0900

    DAY07

--- 12_Polymorphism/src/com/beyond/polymorphism/Application.java
+++ 12_Polymorphism/src/com/beyond/polymorphism/Application.java
@@ -22,7 +22,10 @@ public class Application {
         // 3. 부모 타입의 참조 변수로 자식 타입의 객체를 다루는 경우
         Product television = new Television("a33333", "애플TV","애플", 1800000,24);
 
-        //television 참조 변수로 Product 클래스의 맴버만 접근가능
+        // television 참조 변수로 Product 클래스의 맴버만 접근가능
+        // 단, 자식클래스에서 메소드가 오버라이딩 되었다면 자식클래스의 메소드가 대신 호출된다
         System.out.println(television);
+        // 다시 Television 클래스의 맴버에 접근하고 싶으면 형 변환을 해야한다.
+        System.out.println(((Television)television).getInch());
     }
 }
