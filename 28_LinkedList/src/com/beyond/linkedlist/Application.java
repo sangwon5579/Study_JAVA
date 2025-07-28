d576291b97d14c65f51f5ab12386629d9895d1bf
sangwon <chosw0410@naver.com>
2025-07-28 15:49:18 +0900

    DAY12

--- 28_LinkedList/src/com/beyond/linkedlist/Application.java
+++ 28_LinkedList/src/com/beyond/linkedlist/Application.java
@@ -4,6 +4,8 @@ package com.beyond.linkedlist;
 import com.beyond.linkedlist.practice.List;
 import com.beyond.linkedlist.practice.SinglyLinkedList;
 
+import java.sql.SQLOutput;
+
 public class Application {
     public static void main(String[] args) {
         //java에서 제공하는 linkedlist 테스트
@@ -43,5 +45,59 @@ public class Application {
         System.out.println(list.size());
         System.out.println();
 
+        list.addLast("포도");
+        list.addLast("키위");
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        list.add(1,"수박");
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        list.add(0, "수박");
+        list.add(5, "바나나");
+
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        list.add(4, "복숭아");
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        //get, contains
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(3));
+        System.out.println(list.get(5));
+        System.out.println();
+
+        System.out.println(list.contains("딸기"));
+        System.out.println(list.contains("두리안"));
+        System.out.println(list.contains("참외"));
+        System.out.println(list.contains("복숭아"));
+        System.out.println();
+
+        //removeFirst, removeLast, remove
+        System.out.println(list);
+        list.removeFirst();
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        System.out.println(list);
+        list.removeLast();
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
+
+        System.out.println(list);
+        list.remove(1);
+        System.out.println(list);
+        System.out.println(list.size());
+        System.out.println();
     }
 }
--- 28_LinkedList/src/com/beyond/linkedlist/practice/SinglyLinkedList.java
+++ 28_LinkedList/src/com/beyond/linkedlist/practice/SinglyLinkedList.java
@@ -1,13 +1,13 @@
 package com.beyond.linkedlist.practice;
 
 public class SinglyLinkedList<E> implements List<E> {
-    //연결리스트의 첫번쨰 노드를 가리키는 참조
+    // 연결 리스트의 첫 번째 노드를 가리키는 참조
     private Node<E> head;
 
-    //노드 개수 저장
+    // 노드의 갯수를 저장
     private int size;
 
-    public SinglyLinkedList(){
+    public SinglyLinkedList() {
         this.head = null;
         this.size = 0;
     }
@@ -17,25 +17,57 @@ public class SinglyLinkedList<E> implements List<E> {
         // 새 노드 생성
         Node<E> newNode = new Node<>(data);
 
-        //리스트가 비어있는 경우
-        if(head == null){
+        // 리스트가 비어있는 경우
+        if (head == null) {
             head = newNode;
-        }
-        else{
+        } else {
             newNode.next = head;
             head = newNode;
         }
-        //size 증가
+
+        // size 증가
         size++;
     }
 
     @Override
     public void addLast(E data) {
+        Node<E> newNode = new Node<>(data);
+
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node<E> lastNode = search(size - 1);
+
+            lastNode.next = newNode;
+        }
+
+        size++;
     }
 
     @Override
     public void add(int index, E data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        } else if (index == 0) {
+            this.addFirst(data);
+        } else {
+            Node<E> newNode = new Node<>(data);
+            Node<E> prevNode = search(index - 1);
 
+            newNode.next = prevNode.next;
+            prevNode.next = newNode;
+
+            size++;
+        }
+    }
+
+    @Override
+    public E get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+
+        return search(index).data;
     }
 
     @Override
@@ -45,21 +77,66 @@ public class SinglyLinkedList<E> implements List<E> {
 
     @Override
     public boolean contains(E data) {
+        Node<E> currentNode = head;
+        while (currentNode != null) {
+            if(currentNode.data.equals(data)) {
+                return true;
+            }
+            currentNode = currentNode.next;
+        }
         return false;
     }
 
     @Override
     public void removeFirst() {
+        if(head != null){
+            Node<E> firstNode = head;
 
+            head = firstNode.next;
+            firstNode.data = null;
+            firstNode.next = null;
+            size--;
+        }
     }
 
     @Override
     public void removeLast() {
-
+        if (head != null) {
+            if((size - 2) < 0){
+                this.removeFirst();
+            }
+            else{
+                Node<E> lastPrevNode = search(size-2);
+                lastPrevNode.next.data = null;
+                lastPrevNode.next.next = null;
+                lastPrevNode.next = null;
+                size--;
+            }
+        } else {
+            return;
+        }
     }
 
     @Override
     public void remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException();
+        }
+        if (index == 0){
+            removeFirst();
+            return;
+        }
+        if(index == (size-1)){
+            removeLast();
+            return;
+        }
+
+        Node<E> prevNode = search(index-1);
+        Node<E> targetNode = prevNode.next;
+        prevNode.next = targetNode.next;
+        targetNode.data = null;
+        targetNode.next = null;
+        size--;
 
     }
 
@@ -70,42 +147,48 @@ public class SinglyLinkedList<E> implements List<E> {
 
     @Override
     public String toString() {
-        Node<E> curentNode = head;
+        Node<E> currentNode = head;
         StringBuilder sb = new StringBuilder();
 
         sb.append("[");
 
-        while (curentNode != null) {
-            sb.append(curentNode.data + ", ");
-            curentNode = curentNode.next;
+        while (currentNode != null) {
+            sb.append(currentNode.data).append(", ");
+
+            currentNode = currentNode.next;
         }
-        if( sb.lastIndexOf(",") != -1 ){
+
+        if (sb.lastIndexOf(",") != -1) {
             sb.replace(sb.lastIndexOf(","), sb.length(), "]");
-        }else{
+        } else {
             sb.append("]");
         }
-
-
 
         return sb.toString();
     }
 
-    private static class Node<E>{
-        //노드가 저장하는 데이터
+    private Node<E> search(int index) {
+        Node<E> currentNode = head;
+
+        for (int i = 0; i < index; i++) {
+            currentNode = currentNode.next;
+        }
+
+        return currentNode;
+    }
+
+    // 내부 클래스
+    private static class Node<E> {
+        // 노드가 저장하는 데이터
         private E data;
 
-        //다음 노드를 가리키는 참조
+        // 다음 노드를 가리키는 참조
         private Node<E> next;
 
-
-        public Node(E data){
+        // 노드 객체를 생성할 때 사용할 생성자
+        public Node(E data) {
             this.data = data;
             this.next = null;
         }
-    }
-
-    @Override
-    public E get(int index) {
-        return null;
     }
 }
